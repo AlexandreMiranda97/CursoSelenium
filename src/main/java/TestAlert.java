918b5ac921ee43ab3951e77fd5cebe283ce3d68a
Alexandre Miranda <alexmiranda97.am@gmail.com>
2023-08-08 21:57:20 -0300

    Dry code appliance
    Applying some dry code directives for a best recursive code

--- src/main/java/TestAlert.java
+++ src/main/java/TestAlert.java
@@ -29,27 +29,19 @@ public class TestAlert {
 	public void finish() {
 		driver.quit();
 	}
-	
+
 	@Test
 	public void simpleAlertInteract() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1024, 768));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		driver.findElement(By.id("alert")).click();
 		Alert alert = driver.switchTo().alert();
 		String alertText = alert.getText();
 		Assert.assertEquals("Alert Simples", alert.getText());
 		alert.accept();
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(alertText);
-		driver.quit();
 	}
 
 	@Test
 	public void alertConfirmRefuse() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1024, 768));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-
 //		Alert acceptance and message validation
 		Alert alert = driver.switchTo().alert();
 		driver.findElement(By.id("confirm")).click();
@@ -64,15 +56,11 @@ public class TestAlert {
 		alert.dismiss();
 		Assert.assertEquals("Negado", alert.getText());
 		alert.accept();
-		driver.quit();
 	}
 
 	@Test
 	public void alertPrompt() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1024, 768));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
+
 		driver.findElement(By.id("prompt")).click();
 		Alert alert = driver.switchTo().alert();
 		Assert.assertEquals("Digite um numero", alert.getText());
@@ -82,17 +70,10 @@ public class TestAlert {
 		alert.accept();
 		Assert.assertEquals(":D", alert.getText());
 		alert.accept();
-		
-		driver.quit();		
 	}
-	
+
 	@Test
 	public void fullfilledForm() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().setSize(new Dimension(1024, 768));
-		driver.manage().window().setPosition(new Point(0, 0));
-		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Alexandre");
 		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Miranda da Costa");
 		driver.findElement(By.id("elementosForm:sexo:0")).click();
@@ -100,9 +81,9 @@ public class TestAlert {
 		new Select(driver.findElement(By.id("elementosForm:escolaridade"))).selectByVisibleText("2o grau completo");
 		new Select(driver.findElement(By.id("elementosForm:esportes"))).selectByVisibleText("Natacao");
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Lorem Ipsum Lorem Ipsum Lorem Ipsum");
-		
+
 		driver.findElement(By.id("elementosForm:cadastrar")).click();
-		
+
 		Assert.assertTrue(driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
 		Assert.assertTrue(driver.findElement(By.id("descNome")).getText().endsWith("Alexandre"));
 		Assert.assertTrue(driver.findElement(By.id("descSobrenome")).getText().endsWith("Miranda da Costa"));
@@ -110,8 +91,7 @@ public class TestAlert {
 		Assert.assertEquals("Comida: Pizza", driver.findElement(By.id("descComida")).getText());
 		Assert.assertEquals("Escolaridade: 2graucomp", driver.findElement(By.id("descEscolaridade")).getText());
 		Assert.assertEquals("Esportes: Natacao", driver.findElement(By.id("descEsportes")).getText());
-		Assert.assertEquals("Sugestoes: Lorem Ipsum Lorem Ipsum Lorem Ipsum", driver.findElement(By.id("descSugestoes")).getText());
-		
-		driver.quit();
+		Assert.assertEquals("Sugestoes: Lorem Ipsum Lorem Ipsum Lorem Ipsum",
+				driver.findElement(By.id("descSugestoes")).getText());
 	}
 }
